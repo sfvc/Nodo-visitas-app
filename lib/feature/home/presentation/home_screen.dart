cfae2aff43ac6f4072d3816fce0c112b27cee5bb
Maxi Alzugaray <maxialz99@hotmail.com>
2024-07-10 10:37:46 -0300

    modifico router y pantalla de home para usar widgets

--- lib/feature/home/presentation/home_screen.dart
+++ lib/feature/home/presentation/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nodo_app_2/feature/home/home_wrapper.dart';
+import 'package:nodo_app_2/feature/home/providers/state.provider.dart';
+import 'package:nodo_app_2/shared/shared_wapper.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -27,10 +31,30 @@ class HomeScreen extends StatelessWidget {
               icon: const Icon(Icons.notifications))
         ],
       ),
-      body: Center(
-        child: Text('Home Screen'),
+      body: const SafeArea(
+        child: _HomeBody(),
       ),
-      
+      bottomNavigationBar: const BottomNavigationCommon(),
     );
+  }
+}
+
+class _HomeBody extends ConsumerWidget {
+  const _HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final navigationIndex = ref.watch(bottomNavigationIndexProvider);
+
+    switch (navigationIndex) {
+      case 0:
+        return const HomeContent();
+      case 1:
+        return const SearchContent();
+      case 2:
+        return const WorkersScreen();
+      default:
+        return const HomeContent();
+    }
   }
 }
